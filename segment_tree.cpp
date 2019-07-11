0485cbc85d9359f15aa43646433dddb90f638494
toyama1710 <walker2002226@gmail.com>
2019-07-11 18:44:38 +0900

    verifyyyyyy!!!!!!!!!

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -2,19 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <climits>
 using namespace std;
 
-//===
+// end 63
 template<typename Monoid>
 struct SegmentTree {
     using OP = function<Monoid(Monoid, Monoid)>;
     
     vector<Monoid> tree;
     int size;
-    const op f; // bin' operation
+    const OP f; // bin' operation
     const Monoid e; // neutral element
                                            
-    SegmentTree(int nmemb, const Monoid &e, const op &f):
+    SegmentTree(int nmemb, const Monoid &e, const OP &f):
         f(f),
         e(e)
     {
@@ -27,7 +28,7 @@ struct SegmentTree {
     }
 
     void update(int k, Monoid dat) {
-        k += n - 1;
+        k += size - 1;
         tree[k] = dat;
         
         while(k > 0) {
@@ -38,8 +39,8 @@ struct SegmentTree {
 
     // [l, r)
     Monoid query(int l, int r) {
-        l += n - 1;
-        r += n - 1;
+        l += size - 1;
+        r += size - 1;
 
         Monoid d = e;
         while (l < r) {
@@ -61,6 +62,36 @@ struct SegmentTree {
 };
 //===
 
+//verify AOJ DSL_2_B
+int main()
+{
+    typedef long long ll;
+    
+    ll n, q;
+    ll com, x, y;
+
+    cin >> n >> q;
+
+    SegmentTree<ll> RSQ(n,
+                        0,
+                        [](ll l, ll r){return l + r;});
+
+    for (int i = 0; i < q; i++) {
+        cin >> com >> x >> y;
+        x--;
+
+        if (com == 0) {
+            y += RSQ.query(x, x + 1);
+            RSQ.update(x, y);
+        }
+        else {
+            cout << RSQ.query(x, y) << endl;
+        }
+    }
+}
+
+/*
+  //verify AOJ DSL_2_A
 int main()
 {
     int n, q;
@@ -68,11 +99,11 @@ int main()
 
     cin >> n >> q;
 
-    SegmentTree RMQ<int>(n,
+    SegmentTree<int> RMQ(n,
+                    (1u << 31u) - 1,
                     [](int l, int r){
                         return min(l, r);
-                    },
-                    (1 << 31) - 1);
+                    });
     
     for (int i = 0; i < q; i++) {
         cin >> com >> x >> y;
@@ -87,4 +118,4 @@ int main()
 
     return 0;
 }
-
+*/
