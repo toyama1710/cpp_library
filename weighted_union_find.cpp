d45ccd2c8b2f0759281f725b6f7b78e019cf73b1
toyama1710 <walker2002226@gmail.com>
2019-07-15 13:26:59 +0900

    でぎだ！

--- weighted_union_find.cpp
+++ weighted_union_find.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 using namespace std;
 
 //===
-template<Typename Abel>
-struct UnionFind {
-    vector<int> parent;
-    vector<Abel> diff_wetght; //êeÇ©ÇÁÇÃãóó£
+template<typename Abel = long long>
+struct WeightedUnionFind {
+    using OP = function<Abel(Abel, Abel)>;
 
-    UnionFind(){}
+    vector<int> parent;
+    vector<Abel> diff_weight; //êeÇ©ÇÁÇÃãóó£
+    const OP add;
+    const OP sub;
+    const Abel e;
     
-    UnionFind(int nmemb)
+    WeightedUnionFind(){};
+
+    WeightedUnionFind(int nmemb,
+                      const Abel &e = 0,
+                      const OP &add = [](Abel x, Abel y){return x+y;},
+                      const OP &sub = [](Abel x, Abel y){return x-y;}):
+        add(add),
+        sub(sub),
+        e(e)
     {
-        init(nmemb);
+        parent.assign(nmemb, -1);
+        diff_weight.assign(nmemb, e);
     }
 
     void init(int nmemb)
     {
         parent.clear();
+        diff_weight.clear();
+        
         parent.assign(nmemb, -1);
     }
 
@@ -26,7 +41,12 @@ struct UnionFind {
         if (parent[x] < 0) {
             return x;
         }
-        return parent[x] = root(parent[x]);
+        
+        int p = root(parent[x]);
+        diff_weight[x] = add(diff_weight[x], diff_weight[p]);
+        parent[x] = p;
+
+        return p;
     }
 
     bool same(int x, int y)
@@ -34,17 +54,39 @@ struct UnionFind {
         return root(x) == root(y);
     }
 
-    void unite(int x, int y)
+    
+    Abel weight(int x) {
+        root(x);
+        return diff_weight[x];
+    }
+
+    Abel diff(int x, int y) {
+        return sub(weight(x), weight(y));
+    }
+    
+    bool unite(int x, int y, Abel w)
     {
+        Abel wx = weight(x);
+        Abel wy = weight(y);
+
         x = root(x);
         y = root(y);
 
-        if (x == y) return;
+        if (x == y) return false;
+
+        w = sub(add(w + wx) - wy);
 
         parent[x] += parent[y];
         parent[y] = x;
+        diff_weight[y] = w;
         
-        return;
+        return true;
     }
 };
 //===
+
+int main()
+{
+
+    return 0;
+}
