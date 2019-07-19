06b6bee3ae33e1813a9eb70378f51560a3046378
toyama1710 <walker2002226@gmail.com>
2019-07-19 23:54:33 +0900

    .\

--- lazy_segment_tree.cpp
+++ lazy_segment_tree.cpp
@@ -6,28 +6,41 @@
 using namespace std;
 
 //===
-template<typename Monoid>
-struct SegmentTree {
-    using OP = function<Monoid(Monoid, Monoid)>;
+template<typename Monoid, typename Laz>
+struct LazySegmentTree {
+
+    const function<Monoid(Monoid, Monoid)> mergeMonoid;
+    const function<Monoid(Monoid, Laz)> applyLaz;
+    const function<Laz(Laz, Laz)> mergeLaz;
     
-    vector<Monoid> tree;
+    vector<Monoid> seg;
+    vector<Laz> lazy;
+    vector<bool> isUpdated;
+    
     int size;
-    const OP f; // bin' operation
+    int height;
     const Monoid e; // neutral element
                                            
-    SegmentTree(int nmemb, const Monoid &e, const OP &f):
-        f(f),
-        e(e)
+    LazySegmentTree(int nmemb, const Monoid &e,
+                    function<Monoid(Monoid, Monoid)> f,
+                    function<Monoid(Monoid, Laz)> g,
+                    function<Laz(Laz, Laz)> h):
+        e(e), mergeMonoid(f), applyLaz(g), mergeLaz(h)
     {
+        height = 0;
         size = 1;
         while (size < nmemb) {
             size *= 2;
+            height++;
         }
 
-        tree.assign(2 * size - 1, e);
+        seg.assign(2 * size - 1, e);
+        isUpdated.assign(2 * size - 1, true);
+        lazy.resize(2 * size - 1); 
     }
 
-    void update(int k, Monoid dat)
+    // [l, r) <= dat
+    void update(int l, int r, Laz dat)
     {
         k += size - 1;
         tree[k] = dat;
@@ -41,87 +54,11 @@ struct SegmentTree {
     // [l, r)
     Monoid query(int l, int r)
     {
-        l += size - 1;
-        r += size - 1;
-
-        Monoid d = e;
-        while (l < r) {
-            if (l % 2 == 0) {
-                d = f(d, tree[l]);
-                l++;
-            }
-            if (r % 2 == 0) {
-                r--;
-                d = f(d, tree[r]);
-            }
-
-            l = (l - 1) / 2;
-            r = (r - 1) / 2;
-        }
-
-        return d;
     }
 };
 //===
 
-//verify AOJ DSL_2_B
-int AOJ_DSL_2_B(void)
-{
-    typedef long long ll;
-    
-    ll n, q;
-    ll com, x, y;
-
-    cin >> n >> q;
-
-    SegmentTree<ll> RSQ(n,
-                        0,
-                        [](ll l, ll r){return l + r;});
-
-    for (int i = 0; i < q; i++) {
-        cin >> com >> x >> y;
-        x--;
-
-        if (com == 0) {
-            y += RSQ.query(x, x + 1);
-            RSQ.update(x, y);
-        }
-        else {
-            cout << RSQ.query(x, y) << endl;
-        }
-    }
-}
-
-//verify AOJ DSL_2_A
-int AOJ_DSL_2_A(void)
-{
-    int n, q;
-    int com, x, y;
-
-    cin >> n >> q;
-
-    SegmentTree<int> RMQ(n,
-                    (1u << 31u) - 1,
-                    [](int l, int r){
-                        return min(l, r);
-                    });
-    
-    for (int i = 0; i < q; i++) {
-        cin >> com >> x >> y;
-
-        if (com == 0) {
-            RMQ.update(x, y);
-        }
-        else {
-            cout << RMQ.query(x, y + 1) << endl;
-        }
-    }
-
-    return 0;
-}
-
 int main()
 {
-    //   return AOJ_DSL_2_B();
-    return AOJ_DSL_2_A();
+    return 0;
 }
