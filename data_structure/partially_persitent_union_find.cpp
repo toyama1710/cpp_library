6ce5357a05b971acdc2c9fef93c24cbe838c4954
toyama1710 <walker2002226@gmail.com>
2019-09-17 22:06:58 +0900

    add verify

--- data_structure/partially_persitent_union_find.cpp
+++ data_structure/partially_persitent_union_find.cpp
@@ -61,7 +61,7 @@ struct PartiallyPersistentUnionFind {
 
     int size(int time, int x) {
         x = find(time, x);
-        return (lower_bound(sz[x].begin(), sz[x].end(), make_pair(time, -1))->second);
+        return (upper_bound(sz[x].begin(), sz[x].end(), make_pair(time, 1<<30)) - 1)->second;
     };
 };
 //===
@@ -112,6 +112,54 @@ int CodeThanksFes2017H() {
     return 0;
 }
 
+int AGC002D() {
+    int n, m;
+    int a, b;
+    int q;
+    int x, y, z;
+
+    cin >> n >> m;
+    PartiallyPersistentUnionFind uf(n);
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        a--, b--;
+        
+        uf.unite(a, b);
+    }
+
+    auto f = [&](int t) {
+                 llong xs = uf.size(t, x);
+                 llong ys = uf.size(t, y);
+
+                 if (uf.same(t, x, y)) return xs;
+                 else return xs + ys;
+             };
+
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> x >> y >> z;
+        x--, y--;
+
+        llong valid, invalid;
+        llong mid;
+
+        valid = m;
+        invalid = 0;
+        while (valid - invalid > 1) {
+            mid = (valid + invalid) / 2;
+
+            if (f(mid) >= z) valid = mid;
+            else invalid = mid;
+        }
+        
+        cout << valid << endl;
+    }
+
+    return 0;
+}
+
 int main() {
-    return CodeThanksFes2017H();
+    //return CodeThanksFes2017H();
+    return AGC002D();
 }
