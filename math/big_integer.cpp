c852f2537d5617417bfbf3ab7514855535e1aa38
toyama1710 <walker2002226@gmail.com>
2019-09-28 12:02:46 +0900

    add new file

---
+++ math/big_integer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+using namespace std;
+template<class T, class Compare = less<T> >
+using MaxHeap = priority_queue<T, vector<T>, Compare>;
+template<class T, class Compare = greater<T> >
+using MinHeap = priority_queue<T, vector<T>, Compare>;
+using llong = long long;
+
+struct BigInteger {
+    int len;
+    vector<int> num;
+
+    BigInteger str2num (string s) {
+        BigInteger n;
+
+        
+    }
+};
+
+int main() {
+
+    return 0;
+}
