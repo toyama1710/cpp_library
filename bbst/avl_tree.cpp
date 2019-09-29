b656e14874b2e391df867404596ad0d83bad7c43
toyama1710 <walker2002226@gmail.com>
2019-09-29 17:41:03 +0900

    a

---
+++ bbst/avl_tree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+using namespace std;
+template<class T, class Compare = less<T> >
+using MaxHeap = priority_queue<T, vector<T>, Compare>;
+template<class T, class Compare = greater<T> >
+using MinHeap = priority_queue<T, vector<T>, Compare>;
+using llong = long long;
+
+//===
+
+//===
+
+int main() {
+
+    return 0;
+}
