752e5469984f9ec75ae1757b8393d691afb8e8f7
toyama1710 <walker2002226@gmail.com>
2019-10-05 20:37:34 +0900

    add file

--- bbst/avl_tree.cpp
+++ bbst/avl_tree.cpp
@@ -20,17 +20,44 @@ using llong = long long;
 //===
 template<typename T, typename Compare = less<T> >
 struct AVLTree {
+    enum direction {L = 1, R = 0};
+    direction inv_dir(direction d) {return 1 - d;};
+    
     struct Node {
         T data;
         int height;
-        Node *l, *r;
+        Node *ch[2]; //0:left, 1:right
+
+        Node(T data):data(data), height(0), ch{nullptr, nullptr} {};
     };
 
-    
+    Node *root;
+    Compare cmp;
+
+    AVLTree(Compare cmp = Compare()):root(nullptr), cmp(cmp) {}
+        
+    int height() { return height(root); };
+    int height(Node *p) {
+        return p == nullptr ? -1 : p->height;
+    };
+
+    // d=L:left-rotate; u move to left
+    // d=R:right-rotate;
+    template<int d>
+    Node *rotate(Node *u) {
+        Node w = u->ch[d];
+        u->ch[d] = w->ch[inv_dir(d)];
+        w->ch[inv_dir(d)] = u;
+        // update(u); update(w);
+        return w;
+    };
+
+    void insert(T data) { insert(root, new Node(data)); };
+    Node *insert(Node *r, Node *p, T data) {
+    };
 };
 //===
 
 int main() {
-
     return 0;
 }
