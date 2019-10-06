48d3ef77b326b47556771365988993a1817fccf8
toyama1710 <walker2002226@gmail.com>
2019-10-06 13:18:07 +0900

    Todo:test

---
+++ heap/find_kth.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cassert>
+using namespace std;
+using llong = long long;
+
+//===
+//#include <cassert>
+template<typename T, typename Compare = function<bool(T, T)> >
+struct FindKth {
+    using Heap = priority_queue<T, vector<T>, Compare>;
+    
+    const int K;
+    Heap maxh;
+    Heap minh;
+
+    FindKth (const int K, const Compare &cmp = less<T>()):
+        K(K),
+        maxh(cmp),
+        minh([&cmp](auto l, auto r){ return cmp(r, l); })
+    {};
+    
+    size_t size() { return maxh.size() + minh.size(); };
+    bool empty() { return size() > 0 ? false : true; };
+    
+    void push(T &d){
+        maxh.push(d);
+        if (maxh.size() > K) {
+            minh.push(maxh.top());
+            maxh.pop();
+        }
+    };
+    
+    T find(){
+        assert(maxh.size() == K);
+        return maxh.top();
+    };
+    T find_lower(){
+        assert(!empty());
+        return maxh.top();
+    };
+    
+    void pop() {
+        assert(!empty());
+
+        maxh.pop();
+        if (!minh.empty()) {
+            maxh.push(minh.top());
+            minh.pop();
+        }
+    };
+    
+    void merge_with(FindKth &r) {
+        FindKth &l = *this;
+        if (l.size() < r.size()) swap(l, r);
+
+        while (!r.empty()){
+            l.push(r.find_lower());
+            r.pop();
+        }
+    };
+};
+//===
+
+int yc649() {
+    
+    llong q, k;
+    llong com, v;
+
+    cin >> q >> k;
+    FindKth<llong> st;
+
+    while (q--) {
+        cin >> com;
+
+        if (com == 1) {
+            cin >> v;
+            st.push(v);
+        }
+        else {
+            if (st.size() < k) {
+                cout << -1 << endl;
+            }
+            else {
+                cout << st.find() << endl;
+                st.pop();
+            }
+        }
+    }
+    
+    return 0;
+}
+
+int main() {
+    return yc649();
+}
