ffacf7c4487d78e363ab9f496650df714a822486
toyama1710 <walker2002226@gmail.com>
2019-11-15 21:16:30 +0900

    rename

---
+++ bit/fast_pop_count.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdint>
+using namespace std;
+
+//===
+// #include<cstdint>
+
+#ifdef USE_ASM
+int popcnt32(uint32_t bits) {
+    int ret;
+    __asm__ ("popcntl %[input], %[output]" : [output] "=r"(ret) : [input] "r"(bits));
+    return ret;
+}
+#else
+int popcnt32(uint32_t bits) {
+    bits = (bits & 0x55555555) + (bits >> 1 & 0x55555555);
+    bits = (bits & 0x33333333) + (bits >> 2 & 0x33333333);
+    bits = (bits & 0x0f0f0f0f) + (bits >> 4 & 0x0f0f0f0f);
+    bits = (bits & 0x00ff00ff) + (bits >> 8 & 0x00ff00ff);
+    bits = (bits & 0x0000ffff) + (bits >>16 & 0x0000ffff);
+    return bits;
+}
+#endif
+//===
+
+int main() {
+    uint32_t a;
+
+    cin >> a;
+    cout << popcnt32(a) << endl;
+
+    return 0;
+}
