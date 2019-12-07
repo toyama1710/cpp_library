939952b2003833afd9b541a1bf87f58066eab116
toyama1710 <walker2002226@gmail.com>
2019-12-07 23:52:44 +0900

    add persistent stack

---
+++ stack/persistent_stack.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+using namespace std;
+using llong = long long;
+
+//===
+template<class T, template<class> class Alloc = allocator>
+struct PersistentStack {
+    struct Node {
+        T d;
+        Node *next = nullptr;
+    };
+    using Traits = allocator_traits<Alloc<Node> >;
+    static Alloc<Node> alc;
+
+    Node *head = nullptr;
+
+    PersistentStack current_stack() {
+        return PersistentStack(head);
+    };
+
+    T top() {
+        return head->d;
+    };
+
+    void push(T d) {
+        Node *p = Traits::allocate(alc, 1);
+        Traits::construct(alc, p, d, head);
+        head = p;
+    };
+
+    void pop(T d) {
+        head = head->next;
+    };
+
+    ~PersistentStack() {
+        Node *p;
+        while (head != nullptr) {
+            p = head->next;
+            Traits::deallocate(alc, head, 1);
+            head = p;
+        }
+    };
+};
+//===
+
+int main() {
+
+    return 0;
+}
