79b4c4ca1773600a56c03614ad3bcc03eefbfcd1
toyama1710 <walker2002226@gmail.com>
2020-04-21 13:11:05 +0900

    implement:done verify:none

--- util/memory_pool_allocator.cpp
+++ util/memory_pool_allocator.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <cassert>
+#include <numeric>
+#include <cstdint>
+using namespace std;
 
+//===
+
+// for use: speed up tree (ex. persistent data structure)
 template<class T>
 struct MemoryPoolAllocator {
+    vector<T *> addr;
+    vector<T> mem;
+    size_t ptr;
+
+    MemoryPoolAllocator(size_t n) {
+        mem.resize(n);
+        addr.resize(n);
+
+        for (int i = 0; i < n; i++) {
+            addr[i] = &(mem[i]);
+        }
+    };
+
+    T *allocate(size_t n) {
+        assert(n == 1);
+        return addr[ptr++];
+    };
+
+    void deallocate(T *p, size_t n) {
+        assert(n == 1);
+        addr[--ptr] = p;
+    };
 };
+
+//===
 
 int main() {
 }
