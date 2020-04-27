ab5f8b9f207c020a2b3be1a4240c5d6b9a961e81
root <walker2002226@gmail.com>
2020-04-27 02:36:21 +0000

    bug fix

--- deque/sliding_window.hpp
+++ deque/sliding_window.hpp
@@ -9,81 +9,78 @@
 // #include <cassert>
 template<class SemiGroup, class OP = std::function<SemiGroup(SemiGroup, SemiGroup)> >
 struct SlidingWindow {
+    // first:original data, second:sum
     using Stack = std::stack<std::pair<SemiGroup, SemiGroup> >;
     const OP merge;
 
     Stack front_st, back_st;
-    size_t sz;
 
-    SlidingWindow(const OP &f):merge(f) {
-        sz = 0;
-    };
+    SlidingWindow(const OP &f):merge(f) {};
 
-    SemiGroup fold() {
-        assert(sz > 0);
+    inline SemiGroup fold() {
+        assert(!empty());
         
         if (front_st.empty()) return back_st.top().second;
         else if (back_st.empty()) return front_st.top().second;
         else return merge(front_st.top().second, back_st.top().second);
     };
 
-    void push(SemiGroup d, Stack &front_stack, Stack &back_stack) {
-        sz++;
-        if (back_stack.empty()) back_stack.push({d, d});
-        else back_stack.push({d, merge(back_stack.top().second, d)});
+    inline void push_front(SemiGroup d) {
+        if (front_st.empty()) front_st.emplace(d, d);
+        else front_st.emplace(d, merge(d, front_st.top().second));
     };
-
-    void pop(Stack &front_stack, Stack &back_stack) {
-        assert(!empty());
-        sz--;
-        
-        if (front_stack.empty()) {
-            Stack buff;
-
-            while (buff.size() + 1 < back_stack.size()) {
-                buff.push(back_stack.top());
-                back_stack.pop();
-            }
-
-            while (!back_stack.empty()) {
-                if (front_stack.empty()) front_stack.push({back_stack.top().first,
-                                                           back_stack.top().first});
-                else front_stack.push({back_stack.top().first,
-                                       merge(back_stack.top().first, front_stack.top().second)});
-                back_stack.pop();
-            }
-            
-            while (!buff.empty()) {
-                if (back_stack.empty()) back_stack.push({buff.top().first,
-                                                         buff.top().first});
-                else back_stack.push({buff.top().first,
-                                      merge(back_stack.top().second, buff.top().first)});
-                buff.pop();
-            }
-        }
-        front_stack.pop();
-    };
-
-    void push_front(SemiGroup d) {
-        push(d, back_st, front_st);
-    };
-    void push_back(SemiGroup d) {
-        push(d, front_st, back_st);
+    inline void push_back(SemiGroup d) {
+        if (back_st.empty()) back_st.emplace(d, d);
+        else back_st.emplace(d, merge(back_st.top().second, d));
     };
 
     void pop_front() {
-        pop(front_st, back_st);
+        assert(!empty());
+
+        if (front_st.empty()) {
+            Stack buff;
+            while (buff.size() + 1 < back_st.size()) {
+                buff.push(back_st.top());
+                back_st.pop();
+            }
+            while (!back_st.empty()) {
+                push_front(back_st.top().first);
+                back_st.pop();
+            }
+            while (!buff.empty()) {
+                push_back(buff.top().first);
+                buff.pop();
+            }
+        }
+        front_st.pop();
     };
     void pop_back() {
-        pop(back_st, front_st);
+        assert(!empty());
+
+        if (back_st.empty()) {
+            Stack buff;
+            while (buff.size() + 1 < front_st.size()) {
+                buff.push(front_st.top());
+                front_st.pop();
+            }
+            while (!front_st.empty()) {
+                push_back(front_st.top().first);
+                front_st.pop();
+            }
+            while (!buff.empty()) {
+                push_front(buff.top().first);
+                buff.pop();
+            }
+        }
+        back_st.pop();
     };
     
-    bool empty() {
-        return sz == 0;
+    inline bool empty() {
+        return size() == 0;
     };
 
-    size_t size() {
-        return sz;
+    inline size_t size() {
+        return front_st.size() + back_st.size();
     };
 };
 //===
