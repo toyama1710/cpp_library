c35301b39fd1747f3e6d9215caff132927fad311
root <walker2002226@gmail.com>
2020-04-27 02:40:47 +0000

    add test(swag)

---
+++ test/yosupo/swag.test.cpp
@@ -0,0 +1,63 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+// header file section
+#include <iostream>
+#include <cstdio>
+#include <cfloat>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <algorithm>
+#include "../../deque/sliding_window.hpp"
+
+using namespace std;
+using llong = long long;
+
+//const llong mod = 998244353;
+#define mod (998244353ll)
+SlidingWindow<pair<llong, llong>> swag([](auto l, auto r) {
+            pair<llong, llong> ret;
+            ret.first = l.first * r.first;
+            ret.second = l.first * r.second + l.second;
+            ret.first %= mod;
+            ret.second %= mod;
+            return ret;
+        });
+
+int main() {
+    llong q;
+    llong com;
+    llong a, b;
+    llong x;
+
+    cin >> q;
+
+    while (q--) {
+        cin >> com;
+
+        if (com == 0) {
+            cin >> a >> b;
+            swag.push_front({a, b});
+        }
+        else if (com == 1) {
+            swag.pop_back();
+        }
+        else if (com == 2) {
+            cin >> x;
+
+            if (swag.empty()) {
+                cout << x << '\n';
+            }
+            else {
+                auto f = swag.fold();
+                cout << (f.first * x + f.second) % mod << '\n';
+            }
+        }
+    }
+    return 0;
+};
+
