48e0b15ba6a8a50033b263ddaec9bc4fe76c8753
toyama1710 <walker2002226@gmail.com>
2020-04-27 15:20:07 +0900

    add persistent union-find

---
+++ disjoint_set/persistent_union_find.hpp
@@ -0,0 +1,48 @@
+#ifndef PERSISTENT_UNION_FIND_HPP
+#define PERSISTENT_UNION_FIND_HPP
+
+#include "../array/persistent_array.hpp"
+
+//===
+// 0-indexed
+template<int K = 2>
+struct PersistentUnionFind {
+    PersistentArray<int, K> par;
+
+    
+    PersistentUnionFind(int n): par(n, -1) {};
+    PersistentUnionFind(PersistentArray<int, K> arr):par(arr) {};
+
+    PersistentUnionFind unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+
+        if (x == y) return *this;
+
+        if (size(x) > size(y)) {
+            return par.set(y, x);
+        }
+        else {
+            return par.set(x, y);
+        }
+    };
+
+    int root(int x) {
+        if (par[x] < 0) return x;
+        int res = root(par[x]);
+        return res;
+    };
+
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    };
+
+    // return size of set
+    int size(int x) {
+        x = root(x);
+        return -par[x];
+    };
+};
+//===
+
+#endif
