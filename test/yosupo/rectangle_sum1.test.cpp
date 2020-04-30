0aa60120eba22ee2112e26a1e6d069044a654f0a
root <walker2002226@gmail.com>
2020-04-30 18:31:17 +0000

    add test

---
+++ test/yosupo/rectangle_sum1.test.cpp
@@ -0,0 +1,58 @@
+// header file section
+#include <iostream>
+#include <cstdio>
+#include <cfloat>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <algorithm>
+#include <tuple>
+#include "../../segment_tree/persistent_segment_tree.hpp"
+#include "../../util/coordinate_compression.hpp"
+
+using namespace std;
+using llong = long long;
+
+llong n, q;
+vector<PersistentSegmentTree<llong>> v;
+vector<tuple<llong, llong, llong>> p;
+CoordinateCompression x_axis;
+CoordinateCompression y_axis;
+
+int main() {
+    cin >> n >> q;
+
+    for (int i = 0; i < n; i++) {
+        llong x, y, w;
+        cin >> x >> y >> w;
+        p.push_back(tie(y, x, w));
+        x_axis.add(x);
+        y_axis.add(y);
+    }
+
+    sort(p.begin(), p.end());
+    v.push_back(PersistentSegmentTree<llong>(0,
+                [](auto l, auto r) {return l + r;},
+                x_axis.size()));
+
+    for (int i = 0; i < p.size(); i++) {
+        llong x, y, w;
+        tie(y, x, w) = p[i];
+        x = x_axis.zip(x);
+        y = y_axis.zip(y);
+
+        if (v.size() <= y) {
+            v[y] = v[y - 1];
+        }
+
+        v[y] = v[y].update(x, v[y][x] + w);
+    }
+
+    return 0;
+};
+
