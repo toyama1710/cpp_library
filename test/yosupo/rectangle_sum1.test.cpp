0dac58c9d989b29c0be06624f63d7ac7b79b8c69
root <walker2002226@gmail.com>
2020-04-30 19:17:30 +0000

    add test(coordinate compression, persistent segment tree

--- test/yosupo/rectangle_sum1.test.cpp
+++ test/yosupo/rectangle_sum1.test.cpp
@@ -1,3 +1,4 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/rectangle_sum"
 // header file section
 #include <iostream>
 #include <cstdio>
@@ -36,6 +37,9 @@ int main() {
     }
 
     sort(p.begin(), p.end());
+    x_axis.build();
+    y_axis.build();
+
     v.push_back(PersistentSegmentTree<llong>(0,
                 [](auto l, auto r) {return l + r;},
                 x_axis.size()));
@@ -47,10 +51,27 @@ int main() {
         y = y_axis.zip(y);
 
         if (v.size() <= y) {
-            v[y] = v[y - 1];
+            v.push_back(v[y - 1]);
         }
 
         v[y] = v[y].update(x, v[y][x] + w);
+    }
+
+    for (int i = 0; i < q; i++) {
+        llong l, d, r, u;
+        cin >> l >> d >> r >> u;
+
+        l = x_axis.zip(l);
+        r = x_axis.zip(r);
+        d = y_axis.zip(d) - 1;
+        u = y_axis.zip(u) - 1;
+
+        if (d >= 0) 
+            cout << v[u].fold(l, r) - v[d].fold(l, r) << '\n';
+        else if (u >= 0)
+            cout << v[u].fold(l, r) << '\n';
+        else
+            cout << 0 << '\n';
     }
 
     return 0;
