ee6183d643450829cf2bdec05181c7e149865895
root <walker2002226@gmail.com>
2020-04-30 19:51:10 +0000

    fix const

--- segment_tree/persistent_segment_tree.hpp
+++ segment_tree/persistent_segment_tree.hpp
@@ -25,10 +25,10 @@ struct PersistentSegmentTree {
         Node (Monoid dat):dat(dat) {};
     };
     
-    const Monoid e;
-    const F f;
-    const Node *root;
-    const uint n;
+    Monoid e;
+    F f;
+    Node *root;
+    uint n;
 
     Node *merge_node(Node *lch, Node *rch) {
         Monoid l = (lch ? lch->dat : e);
@@ -40,33 +40,37 @@ struct PersistentSegmentTree {
         return ret;
     };
 
+    PersistentSegmentTree(const PersistentSegmentTree &) = default;
     PersistentSegmentTree(Monoid e, F f, uint n, Node *r)
         :e(e), f(f), n(n), root(r) {};
     PersistentSegmentTree(Monoid e, F f, uint n)
         :e(e), f(f), n(n), root(alloc(0, n, std::vector<Monoid>(n, e))) {};
     template<class InputItr>
-    PersistentSegmentTree(Monoid e, F f, InputItr first, InputItr last)
+    PersistentSegmentTree(Monoid e, F f, const InputItr first, const InputItr last)
         :e(e), f(f), n(std::distance(first, last)), root(alloc(0, n, std::vector<Monoid>(first, last))) {};
 
     Node *alloc(uint nl, uint nr, const std::vector<Monoid> &v) {
         if (nr - nl <= 1) return new Node(v[nl]);
         else return merge_node(alloc(nl, (nl + nr) / 2, v),
-                            alloc((nl + nr) / 2, nr, v));
+                alloc((nl + nr) / 2, nr, v));
     };
 
     const Monoid fold(uint l, uint r) const {
         return fold(l, r, 0, n, root);
     };
-    const Monoid fold(uint ql, uint qr, uint nl, uint nr, Node *np) const {
+    const Monoid fold(uint ql, uint qr, uint nl, uint nr, const Node *np) const {
         if (np == nullptr || qr <= nl || nr <= ql) return e;
         else if (ql <= nl && nr <= qr) return np->dat;
         else return f(fold(ql, qr, nl, (nl + nr) / 2, np->l), fold(ql, qr, (nl + nr) / 2, nr, np->r));
     };
 
+    PersistentSegmentTree update(uint idx, Monoid d) {
+        return set(idx, d);
+    };
     PersistentSegmentTree set(uint idx, Monoid d) {
         return PersistentSegmentTree(e, f, n, update(0, n, idx, d, root));
     };
-    Node *update(uint nl, uint nr, uint idx, Monoid d, const Node *np) {
+    Node *update(uint nl, uint nr, uint idx, Monoid d, Node *np) {
         if (idx < nl || nr <= idx) return np;
         else if (nr - nl == 1) return new Node(d);
         else return merge_node(update(nl, (nl + nr) / 2, idx, d, np->l), update((nl + nr) / 2, nr, idx, d, np->r));
@@ -76,9 +80,11 @@ struct PersistentSegmentTree {
         return *this;
     };
 
-    const Monoid operator [] (uint idx) const {
+    Monoid operator [] (uint idx) {
         return fold(idx, idx + 1, 0, n, root);
     };
+
+    PersistentSegmentTree &operator = (const PersistentSegmentTree &) = default;
 };
 //===
 
