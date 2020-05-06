ea675fff718c4f8bdc280504fabbf52b9fee4d75
root <walker2002226@gmail.com>
2020-05-06 11:00:35 +0000

    change design

--- segment_tree/segment_tree.hpp
+++ segment_tree/segment_tree.hpp
@@ -13,14 +13,14 @@ struct SegmentTree {
     std::vector<T> tree;
 
     SegmentTree() = default;
-    SegmentTree(int n)
-        :tree(size << 1, Monoid::identity()) {};
+    explicit SegmentTree(int n)
+        :tree(n << 1, Monoid::identity()) {};
 
     template<class InputIterator>
     SegmentTree(InputIterator first, InputIterator last) {
-        tree.assign(size << 1, Monoid::identity());
-        int i;
+        tree.assign(distance(first, last) << 1, Monoid::identity());
 
+        int i;
         i = size;
         for (InputIterator itr = first; itr != last; itr++) {
             tree[i++] = *itr;
@@ -35,12 +35,12 @@ struct SegmentTree {
         return tree.size() >> 1;
     };
 
-    inline T operator[] (const int k) const {
-        return tree[k + size];
+    inline T operator[] (int k) {
+        return tree[k + size()];
     };
 
-    void update(int k, T dat) {
-        k += size;
+    void update(int k, const T dat) {
+        k += size();
         tree[k] = dat;
         
         while(k > 1) {
@@ -51,11 +51,11 @@ struct SegmentTree {
 
     // [l, r)
     T fold(int l, int r) {
-        l += size; //points leaf
-        r += size;
+        l += size(); //points leaf
+        r += size();
 
-        Monoid lv = Monoid::identity();
-        Monoid rv = Monoid::identity();
+        T lv = Monoid::identity();
+        T rv = Monoid::identity();
         while (l < r) {
             if (l & 1) lv = Monoid::operation(lv, tree[l++]);
             if (r & 1) rv = Monoid::operation(tree[--r], rv);
