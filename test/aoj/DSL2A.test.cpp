f9fabdccfcf1e6b58e635beb750f18313050663b
toyama1710 <walker2002226@gmail.com>
2020-05-07 13:19:34 +0900

    fix

--- test/aoj/DSL2A.test.cpp
+++ test/aoj/DSL2A.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
 
 // header file section
 #include <iostream>
