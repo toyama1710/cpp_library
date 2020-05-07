729b1d13b0823f71e8540bce1caa00f156d031d5
toyama1710 <walker2002226@gmail.com>
2020-05-07 14:44:00 +0900

    add file

---
+++ data_type/max_monoid.hpp
@@ -0,0 +1,18 @@
+#ifndef MAX_MONOID_HPP
+#define MAX_MONOID_HPP
+
+#include <numeric>
+#include <algorithm>
+
+template<class T>
+struct MinMonoid {
+    using value_type = T;
+    inline static T identity() {
+        return std::numeric_limits<T>::min();
+    };
+    inline static T operation(const T a, const T b) {
+        return std::max(a, b);
+    };
+};
+
+#endif
