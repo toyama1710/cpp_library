98ea5d02ee81eba2931e3cc0157c6e8637509958
toyama1710 <walker2002226@gmail.com>
2020-06-02 23:29:58 +0900

    e, wakaran yameta

--- segment_tree/lazy_segment_tree.hpp
+++ segment_tree/lazy_segment_tree.hpp
@@ -60,7 +60,7 @@ struct LazySegmentTree {
         uint32_t tmpl = l;
         uint32_t tmpr = r;
         push_down(l);
-        push_down(r);
+        push_down(r - 1);
 
         while (l < r) {
             if (l & 1) {
@@ -78,7 +78,7 @@ struct LazySegmentTree {
         }
 
         recalc(tmpl);
-        recalc(tmpr);
+        recalc(tmpr - 1);
     };
 
     // foldl[l, r)
