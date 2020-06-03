de5afbdc3a5f1bf3ec8eb8d33e1a1d9d39c711df
root <walker2002226@gmail.com>
2020-06-03 06:36:32 +0000

    add test

---
+++ test/yosupo/union_find.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+// header file section
+#include <iostream>
+#include <cstdio>
+#include <cfloat>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <algorithm>
+#include "../../disjoint_set/union_find.hpp"
+
+using namespace std;
+using llong = long long;
+
+llong n, q;
+llong com, u, v;
+UnionFind uf;
+
+int main() {
+    cin >> n >> q;
+    uf.init(n);
+
+    while (q--) {
+        cin >> com >> u >> v;
+
+        if (com == 0) {
+            uf.unite(u, v);
+        }
+        else {
+            cout << uf.same(u, v) ? 1 : 0 << '\n';
+        }
+    }
+
+    return 0;
+};
+
