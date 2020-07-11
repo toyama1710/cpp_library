871f3d9a14009446b4bc8b4056079ee9c900ae70
root <walker2002226@gmail.com>
2020-07-11 08:36:24 +0000

    add lct

--- cht/li_chao_tree.hpp
+++ cht/li_chao_tree.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <numeric>
+#include <iterator>
 
 template <class T = long long>
 struct LiChaoTree {
@@ -20,23 +22,33 @@ struct LiChaoTree {
     };
 
     std::vector<Line> seg;
+    std::vector<T> pos;
 
     LiChaoTree() = default;
-    explicit LiChaoTree(int n):seg(n << 1, Line::identity()) {};
+    explicit LiChaoTree(int n):seg(n << 1, Line::identity()), pos(n) {
+        std::iota(pos.begin(), pos.end(), 0);
+    };
+    template<class InputItr>
+    LiChaoTree(InputItr first, InputItr last):
+        seg(std::distance(first, last) << 1, Line::identity()), pos(first, last)
+    {};
 
     int size() {
         return  seg.size() >> 1;
     };
 
-    void add_segment(T a, T b, int l, int r) {
+    void add_line(T a, T b) {
+        add_segment(a, b, pos[0], pos.back());
+    };
+    void add_segment(T a, T b, T l, T r) {
         Line x(a, b);
         int ll = l, lr = l + 1;
         int rl = r - 1, rr = r;
-        l += size();
-        r += size();
+        l = std::lower_bound(pos.begin(), pos.end(), l) - pos.begin() + size();
+        r = std::lower_bound(pos.begin(), pos.end(), r) - pos.begin() + size();
         while (l < r) {
             if (l & 1) {
-                update(x, l, ll, lr);
+                update(x, l, pos[ll], pos[lr]);
                 l++;
                 int range = lr - ll;
                 ll += range;
@@ -44,7 +56,7 @@ struct LiChaoTree {
             }
             if (r & 1) {
                 r--;
-                update(x, r, rl, rr);
+                update(x, r, pos[rl], pos[rr]);
                 int range = rr - rl;
                 rl -= range;
                 rr -= range;
@@ -57,17 +69,17 @@ struct LiChaoTree {
     };
 
     void update(Line &x, int k, int l, int r) {
-        //replace
-        if (x.get(l) <= seg[k].get(l) && x.get(r - 1) <= seg[k].get(r - 1)) seg[k] = x;
-        //left
-        else if (x.get(l) < seg[k].get(l)) update(x, k << 1, l, (l + r) >> 1);
-        //right
-        else if (x.get(r - 1) < seg[k].get(r - 1)) update(x, (k << 1) | 1, (l + r) >> 1, r);
+        int pl = pos[l];
+        int pr = pos[r - 1];
+        if (x.get(pl) <= seg[k].get(pl) && x.get(pr) <= seg[k].get(pr)) seg[k] = x;
+        else if (x.get(pl) < seg[k].get(pl)) update(x, k << 1, l, (l + r) >> 1);
+        else if (x.get(pr) < seg[k].get(pr)) update(x, (k << 1) | 1, (l + r) >> 1, r);
         else return;
     };
 
     T get(int x) {
-        int k = x + size();
+        int k = std::lower_bound(pos.begin(), pos.end(), x) - pos.begin() + size();
+
         T ret = seg[k].get(x);
         while (k > 0) {
             k >>= 1;
