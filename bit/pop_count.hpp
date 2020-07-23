436730221464f916276a83137ab66e0dbe6c570d
toyama1710 <walker2002226@gmail.com>
2020-07-23 12:06:47 +0900

    add file

---
+++ bit/pop_count.hpp
@@ -0,0 +1,34 @@
+#ifndef POP_COUNT_HPP
+#define POP_COUNT_HPP
+
+#include <cstdint>
+
+//===
+int popcnt64(uint64_t bits) {
+#ifdef __has_builtin
+    return __builtin_popcountll(bits);
+#else
+    bits = (bits & 0x5555555555555555) + (bits >> 1  & 0x5555555555555555);
+    bits = (bits & 0x3333333333333333) + (bits >> 2  & 0x3333333333333333);
+    bits = (bits & 0x0f0f0f0f0f0f0f0f) + (bits >> 4  & 0x0f0f0f0f0f0f0f0f);
+    bits = (bits & 0x00ff00ff00ff00ff) + (bits >> 8  & 0x00ff00ff00ff00ff);
+    bits = (bits & 0x0000ffff0000ffff) + (bits >> 16 & 0x0000ffff0000ffff);
+    bits = (bits & 0x00000000ffffffff) + (bits >> 32 & 0x00000000ffffffff);
+    return bits;
+#endif
+}
+int popcnt32(uint32_t bits) {
+#ifdef __has_builtin
+    return __builtin_popcount(bits);
+#else
+    bits = (bits & 0x55555555) + (bits >> 1 & 0x55555555);
+    bits = (bits & 0x33333333) + (bits >> 2 & 0x33333333);
+    bits = (bits & 0x0f0f0f0f) + (bits >> 4 & 0x0f0f0f0f);
+    bits = (bits & 0x00ff00ff) + (bits >> 8 & 0x00ff00ff);
+    bits = (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff);
+    return bits;
+#endif
+}
+//===
+
+#endif
---
+++ util/xorshift.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+#include <ctime>
+
+//===
+uint32_t xorshift32() {
+    static uint32_t s = 1710;
+    s = s ^ (s << 13);
+    s = s ^ (s >> 17);
+    s = s ^ ( s << 5);
+    return s;
+}
+//===
