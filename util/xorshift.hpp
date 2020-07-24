d594a679d0053cc87fe1ac8646d72355dc4eb2bc
toyama1710 <walker2002226@gmail.com>
2020-07-25 00:09:57 +0900

    impl set seed

--- util/xorshift.hpp
+++ util/xorshift.hpp
@@ -3,11 +3,14 @@
 #include <ctime>
 
 //===
+static uint32_t __seed__ = 1710;
 uint32_t xorshift32() {
-    static uint32_t s = 1710;
-    s = s ^ (s << 13);
-    s = s ^ (s >> 17);
-    s = s ^ (s << 5);
-    return s;
+    __seed__ = __seed__ ^ (__seed__ << 13);
+    __seed__ = __seed__ ^ (__seed__ >> 17);
+    __seed__ = __seed__ ^ (__seed__ << 5);
+    return __seed__;
 }
+void set_seed(uint32_t s) {
+    __seed__ = s;
+};
 //===
