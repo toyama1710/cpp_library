9db33080e4e84afedd72e3566a01bfd4749d72f6
toyama1710 <toyama@toyama1710.net>
2021-05-02 16:52:58 +0900

    add test(persitent lazy segtree)

--- test/aoj/DSL2F_2.test.cpp
+++ test/aoj/DSL2F_2.test.cpp
@@ -6,7 +6,7 @@
 #define _overload(_1, _2, _3, _4, name, ...) name
 #define _rep1(Itr, N) _rep3(Itr, 0, N, 1)
 #define _rep2(Itr, a, b) _rep3(Itr, a, b, 1)
-#define _rep3(Itr, a, b, step) for (i64(Itr) = a; (Itr) < b; (Itr) += step)
+#define _rep3(Itr, a, b, step) for (i64 Itr = a; Itr < b; Itr += step)
 #define repeat(...) _overload(__VA_ARGS__, _rep3, _rep2, _rep1)(__VA_ARGS__)
 #define rep(...) repeat(__VA_ARGS__)
 
