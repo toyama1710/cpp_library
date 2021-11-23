854b6f848e51569bf0b4306611c670946c707ce1
toyama1710 <toyama@toyama1710.net>
2021-11-23 23:13:24 +0900

    add mod_binomial

---
+++ math/mod_binomial.hpp
@@ -0,0 +1,29 @@
+#ifndef MOD_BINOMIAL_HPP
+#define MOD_BINOMIAL_HPP
+
+#include <cassert>
+#include <cstdint>
+
+#include "modint.hpp"
+
+// nCr mod p
+// p is prime
+template <uint64_t p>
+ModInt<p> binomial(uint64_t n, uint64_t r) {
+    assert(r <= n);
+    static std::vector<ModInt<p>> fact;
+    auto f = [&](auto &&f, uint64_t n) -> ModInt<p> {
+        if (fact.size() > n) return fact[n];
+
+        if (n == 0)
+            fact.push_back(1);
+        else
+            fact.push_back(f(f, n - 1) * n);
+
+        return fact[n];
+    };
+
+    return f(f, n) / f(f, r) / f(f, n - r);
+};
+
+#endif
---
+++ test/aoj/1501.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1501"
+#include <algorithm>
+#include <iostream>
+
+#include "../../math/mod_binomial.hpp"
+#include "../../math/modint.hpp"
+
+using namespace std;
+
+int main() {
+    int r, c, a1, a2, b1, b2;
+    cin >> r >> c >> a1 >> a2 >> b1 >> b2;
+
+    int h1 = abs(b1 - a1);
+    int h2 = r - abs(b1 - a1);
+    int w1 = abs(b2 - a2);
+    int w2 = c - abs(b2 - a2);
+
+    int h = min(h1, h2);
+    int w = min(w1, w2);
+
+    const int p = 100'000'007;
+    auto ans = binomial<p>(h + w, h);
+    if (h1 == h2) ans *= 2;
+    if (w1 == w2) ans *= 2;
+
+    cout << ans << endl;
+
+    return 0;
+}
